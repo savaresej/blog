f5c712dc9bf08698c416d527888e59c8b08b9307
Joseph Savarese <savaresej@joseph-savareses-macbook-pro.local>
2014-07-30 20:13:17 -0400

    first major changes

---
+++ app/views/welcome/name.rb
@@ -0,0 +1,12 @@
+name1 = "Joe"
+name2 = "Robert"
+if name1.length > name2.length
+	puts "name1"
+else 
+	if name1.length == name2.length
+		puts "They're the same length!"
+		else
+		puts "name2"
+	end
+end
+
