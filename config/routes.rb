52d16922f65a34835ded5ca7b4f62e542d87d9d2
Your Namesavaresej@joseph-savareses-macbook-pro.local <savaresej@joseph-savareses-macbook-pro.local>
2014-08-04 20:58:31 -0400

    routes / welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+root 'welcome#index'
+
+  get 'sitemapping/sitemap'
+
+  get 'aboutme/about'
+
+  get 'sitemap/index'
+
+  get 'welcome' => 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
