647354a5f8052d945a7ccc6d4694d9df6703f7f1
Your Namesavaresej@joseph-savareses-macbook-pro.local <savaresej@joseph-savareses-macbook-pro.local>
2014-08-11 19:46:58 -0400

    added images to assets folder

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
 root 'welcome#index'
 
   get 'aboutme' => 'aboutme#index'
-
   get 'sitemap' => 'sitemap#index'
-
   get 'welcome' => 'welcome#index'
+  get 'aboutme/pics' => 'aboutme#pics'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
