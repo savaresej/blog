1cb1e38f4c41929456e8e4e4da3b3bd181e3ddb8
Your Namesavaresej@joseph-savareses-macbook-pro.local <savaresej@joseph-savareses-macbook-pro.local>
2014-08-11 20:40:45 -0400

    added cookies for first visitr

--- app/controllers/aboutme_controller.rb
+++ app/controllers/aboutme_controller.rb
@@ -2,9 +2,6 @@ class AboutmeController < ApplicationController
   def index
   end
   def pics
-  	@imgData = {"Porsche 911" => ["porsche-exclusive-911t-cab.jpg", "http://o.aolcdn.com/hss/storage/adam/6e58b30fb96bf0d28d10fa6d290682e7/porsche-exclusive-911t-cab.jpg"],
-	"Ferrari F12" => ["ferrari-f12-berlinetta-front-quarter.jpg", "http://www.autotribute.com/wp-content/gallery/ferrari-f12-berlinetta-vs-lamborghini-aventador/ferrari-f12-berlinetta-front-quarter.jpg"],
-	"BAC Mono" => ["img_bac_mono_car_3.jpg", "http://www.blessthisstuff.com/imagens/stuff/img_bac_mono_car_3.jpg"]} 
-
+  	@imgData = {"Porsche 911" => "porsche-exclusive-911t-cab.jpg", "Ferrari F12" => "ferrari-f12-berlinetta-front-quarter.jpg", "BAC Mono" => "img_bac_mono_car_3.jpg"}
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+  	# get current date and time and create a string version of date and time
+  	current_dt = Time.now.strftime("%B %d, %Y, %A %H:%M")
+
+  	# store the current date and time in the session
+  	if session['first_visit'] == nil
+  		session["first_visit"] = current_dt
+  	end
+  	
+  	# pull the date and time from the session and make it available to the view
+  	@first_visit = session["first_visit"]
   end
 end
