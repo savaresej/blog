233cb6674f2a1ba04ba165546e67063b59c325e8
Your Namesavaresej@joseph-savareses-macbook-pro.local <savaresej@joseph-savareses-macbook-pro.local>
2014-08-20 23:13:53 -0400

    added picture database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819003558) do
+ActiveRecord::Schema.define(version: 20140821024942) do
+
+  create_table "pictures", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "name"
